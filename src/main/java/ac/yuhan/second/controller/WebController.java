1b6097713237806be18ec2e7e3e12b6710195212
iathorn <kkiathorn@gmail.com>
2018-04-04 16:52:20 +0900

    first commit

---
+++ src/main/java/ac/yuhan/second/controller/WebController.java
@@ -0,0 +1,13 @@
+package ac.yuhan.second.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class WebController {
+    @RequestMapping(value="/signup", method=RequestMethod.GET)
+    public String index(){
+        return "signup";
+    }
+}
